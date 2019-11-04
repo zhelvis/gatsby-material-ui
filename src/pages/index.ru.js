6d08abf4e8d98ba010e17caab9bab168fbbe6110
Vladimir Zhelvis <war1lok@yandex.ru>
2019-11-05 02:27:17 +0300

    add gatsby-i18n-plugin

---
+++ src/pages/index.ru.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Button from "@material-ui/core/Button"
+import DarkModeContext from '../theme/DarkModeContext'
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <DarkModeContext.Consumer>
+        {({ toogleDarkMode }) => (
+          <Button onClick={toogleDarkMode} variant="contained" color="primary">
+            Test
+          </Button>
+        )}
+      </DarkModeContext.Consumer>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+export default IndexPage
